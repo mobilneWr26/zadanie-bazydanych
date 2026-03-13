b768b2629082d44ef0379a6bf5d49592d0772f5e
mobilneWr26 <wiktor.rojek@uczen.mechaniktg.pl>
2026-03-13 10:50:26 +0100

    layout oraz pobranie i przypisanie zmiennych

--- app/src/main/java/com/example/zadanie_bazydanych/MainActivity.java
+++ app/src/main/java/com/example/zadanie_bazydanych/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.zadanie_bazydanych;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +11,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText tytul;
+    private EditText autor;
+    private EditText cena;
+    private EditText iloscStron;
+    private Button dodajButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tytul = findViewById(R.id.tytul);
+        autor = findViewById(R.id.autor);
+        cena = findViewById(R.id.cena);
+        iloscStron = findViewById(R.id.iloscStron);
+        dodajButton = findViewById(R.id.dodaj);
     }
 
 }
