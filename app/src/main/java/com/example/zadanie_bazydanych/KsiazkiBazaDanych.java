53cd3f21510829511e1c0bbec685f41664b2929a
mobilneWr26 <wiktor.rojek@uczen.mechaniktg.pl>
2026-03-13 11:42:51 +0100

    klasa KsiazkiBazaDanych

--- app/src/main/java/com/example/zadanie_bazydanych/KsiazkiBazaDanych.java
+++ app/src/main/java/com/example/zadanie_bazydanych/KsiazkiBazaDanych.java
@@ -1,6 +1,18 @@
 package com.example.zadanie_bazydanych;
+import android.content.Context;
 
-public class KsiazkiBazaDanych {
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
+@Database( entities = {Ksiazki.class}, version = 2)
+public abstract class KsiazkiBazaDanych extends RoomDatabase{
+    public abstract KsiazkiDao zwrocKsiazkiDao();
+    private static KsiazkiBazaDanych instancja;
+    public static KsiazkiBazaDanych zwrocInstancjeBazyDanych(Context context){
+        if(instancja == null){
+            instancja = Room.databaseBuilder(context, KsiazkiBazaDanych.class, "ksiazki_db").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+        }
+        return instancja;
+    }
 }
-
